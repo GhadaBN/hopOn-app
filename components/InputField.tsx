b54b3c83ffa4e3a9731b0296dc9d811c5e3e56ee
Ghada Bennasr <gh.bennasr@gmail.com>
2024-12-24 23:05:42 +0100

    fix error

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,10 +1,5 @@
-import {
-  KeyboardAvoidingView,
-  TouchableWithoutFeedback,
-  View,
-  Text,
-  Image,
-} from "react-native";
+import React from "react";
+import { View, Text, TextInput, Image } from "react-native";
 
 const InputField = ({
   label,
@@ -14,26 +9,29 @@ const InputField = ({
   containerStyle,
   inputStyle,
   iconStyle,
-  className,
   ...props
 }) => (
-  <KeyboardAvoidingView>
-    <TouchableWithoutFeedback>
-      <View className="my-2 w-full">
-        <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle}`}>
-          {label}
-        </Text>
-        <View
-          className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-primary-500 ${containerStyle}`}
-        >
-          {" "}
-          {icon && (
-            <Image source={icon} className={`w-6 h-6 ml-4 ${iconStyle}`} />
-          )}
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
-  </KeyboardAvoidingView>
+  <View className="my-2 w-full">
+    {label && (
+      <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle || ""}`}>
+        {label}
+      </Text>
+    )}
+    <View
+      className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-primary-500 ${
+        containerStyle || ""
+      }`}
+    >
+      {icon && (
+        <Image source={icon} className={`w-6 h-6 ml-4 ${iconStyle || ""}`} />
+      )}
+      <TextInput
+        secureTextEntry={secureTextEntry}
+        className={`flex-1 px-4 py-2 ${inputStyle || ""}`}
+        {...props}
+      />
+    </View>
+  </View>
 );
 
 export default InputField;
